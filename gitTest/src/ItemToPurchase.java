7c484554932da18ef9b28dc7509b2df65a732824
njmsk <njmsk@DESKTOP-I0B7DUS.mit.edu>
2020-10-16 18:02:59 +0800

    create instance variable

---
+++ gitTest/src/ItemToPurchase.java
@@ -0,0 +1,12 @@
+
+public class ItemToPurchase {
+	private String itemName;
+	private int itemPrice;
+	private int itemQuantity;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
